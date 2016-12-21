221d618e0b091648d761788aa003a3e80672b451
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2016-12-21 13:51:36 -0200

    done view and template for detail

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from .models import Question
 
