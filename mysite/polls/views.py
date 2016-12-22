8ddaf80a14b9f598d7b9d9d74e43fabd90a54c57
Rafael Goncalves <rafaelgoncalves258@gmail.com>
2016-12-22 09:39:28 -0200

    added css stylesheet to the polls index page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse, timezone
+from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Question, Choice
